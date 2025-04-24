31ea337e59b185d96504f0db7f7abb668ccad7f2
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-04-24 17:46:22 +0530

    Update API_KEY.js

--- API_KEY.js
+++ API_KEY.js
@@ -1,2 +1,2 @@
-//export const KEY = 'AIzaSyBLoG7_zNbC4Ytrqx8BqYGQ_3ivC1Fcd60'; // gulamnabi9th key
-export const KEY = 'AIzaSyCodTw4JPUwycyR_beSD_rYlh4vTmXc6Ao';  // 43@gapruluniversity k
+
+export const KEY = process.env.KEY;  
